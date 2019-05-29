73a5c1e675d77d65cf669628ff163acbee634a32
YDDDD6 <2522471070@qq.com>
2019-05-29 21:43:58 +0800

    更新GetContent，使新闻显示不全的信息信息补全

--- MyReptile/AhstuNews/MyGetContent/ReptileContent.py
+++ MyReptile/AhstuNews/MyGetContent/ReptileContent.py
@@ -16,7 +16,17 @@ class GetContent(object):
         self.urlxsxx = 'http://www.ahstu.edu.cn/index/xsxx.htm'  # 学术信息
         self.urlmtak = 'http://www.ahstu.edu.cn/index/mtak.htm'  # 媒体安科
 
-    def GetContent(self, url):
+    def GetNewsContent(self, url):
+        """或取新闻具体内容"""
+        content = requests.get(url)
+        content.encoding = 'utf-8'
+        contentsoup = BeautifulSoup(content.text, 'lxml')
+        newstitle = contentsoup.find('div', attrs={'class': 'list-textT'})
+        if newstitle:
+            return newstitle.string
+        return None
+
+    def GetNews(self, url):
         content = requests.get(url)  # 爬取内容
         content.encoding = 'utf-8'
         contentsoup = BeautifulSoup(content.text, 'lxml')  # beautifulsoup4分析
@@ -24,9 +34,11 @@ class GetContent(object):
         for link in contentsoup.find_all('a', attrs={'class': 'rigthConBox-conList'}):
             spanlist = link.find_all('span', attrs={'class': ['conListWord', 'conListTime']})
             newsurl = link.get('href')
-            if (re.match('\.\.', newsurl)):
+            if re.match('\.\.', newsurl):
                 newsurl = self.head+newsurl[2:]
-            newsname = spanlist[0].string
+            newsname = self.GetNewsContent(newsurl)
+            if not newsname:
+                newsname = spanlist[0].string
             newsdate = spanlist[1].string
             newsdict[newsname] = [newsurl, newsdate]
         return newsdict
