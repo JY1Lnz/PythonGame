9b7bc1e9bff5bac2d1512cfc835efa70a5ba9cd4
YDDDD6 <2522471070@qq.com>
2019-06-14 15:10:29 +0800

    Update

--- MyReptile/AhstuNews/use.py
+++ MyReptile/AhstuNews/use.py
@@ -5,11 +5,14 @@ from MyReptile.AhstuNews.MyGetContent import ReptileContent
 getcontent = ReptileContent.GetContent()
 newsdict = getcontent.getstart()
 print("获取结束，开始分析")
+number = 0
 for key in newsdict:
     if re.search('信息', key):
+        number += 1
         print(key)
         filename = 'newscontent/'+key+'.txt'
         news = open(filename, 'w', encoding='utf-8')
         newscontent = getcontent.gtnewscontent(newsdict[key][0])
         print(newscontent, file=news)
         news.close()
+print("共"+str(number)+"条信息")
